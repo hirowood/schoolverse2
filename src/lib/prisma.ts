81d3e03b6c30185dacb637cb557b165794c44094
hirowood <hirowoodandmika730@gmail.com>
2025-11-26 16:44:57 +0900

    Explicitly set engineType binary in PrismaClient constructor

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -13,6 +13,7 @@ const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    engineType: "binary",
     log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
